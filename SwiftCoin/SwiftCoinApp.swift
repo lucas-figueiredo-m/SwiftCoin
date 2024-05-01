ad799d4e8381ca2a0aadd2c08bdf03510cef1b2b
Lucas Figueiredo <lfigueiredo@upstart13.com>
2024-05-01 19:34:01 -0300

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Lucas Martins Figueiredo on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
